d11eb507975899d05274d22de531d9b36efeabba
cae67 <51794040+cae67@users.noreply.github.com>
2024-02-19 14:04:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -63,6 +63,12 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 
     # What does your project relate to?
